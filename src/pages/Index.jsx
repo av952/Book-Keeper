2c4875d4eb65033244251a4027095ffc26643b04
av952 <av952@github.com>
2022-09-23 20:08:13 -0500

    details

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,14 +5,21 @@ import '../style.css'
 
 export const Index = () => {
   const store = useAppContext();
-  
+
+  console.log(store.items.length);  
   return (
     <Layout>
+      {
+        store.items.length >1
+        ?
       <div className="containerIndex">
         {store.items.map((el) => (
           <Book item={el} key={el.id} />
         ))}
       </div>
+      :
+      <h1 className="msg">No hay datos aún, crea nuevos datos desde la pestaña de <strong> create </strong> </h1>
+      }
     </Layout>
   );
 };
